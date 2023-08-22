1de49376a27f383ccd0bb06a0fa74bdc7353cade
med mk <med.mbarek.isi@gmail.com>
2023-08-22 11:44:30 +0200

    The new component for chat with a new design Also retrieving the data from the backend for sidebar users

--- src/scenes/chat1/index.jsx
+++ src/scenes/chat1/index.jsx
@@ -1,165 +1,156 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import { Box } from "@mui/material";
-import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import {
-    BasicStorage,
-    ChatMessage,
-    ChatProvider,
-    Conversation,
-    ConversationId,
-    ConversationRole,
-    IStorage,
-    MessageContentType,
-    Participant,
-    Presence,
-    TypingUsersList,
-    UpdateState,
-    User,
-    UserStatus
-} from "@chatscope/use-chat";
 import { useParams } from 'react-router-dom';
-
-import {ExampleChatService} from "@chatscope/use-chat/dist/examples";
-import {Chat} from "./components/Chat";
-import {nanoid} from "nanoid";
-import {AutoDraft} from "@chatscope/use-chat/dist/enums/AutoDraft";
-
-
+import { Box, useTheme, Typography, Paper, Grid, Divider, TextField, List, ListItem, ListItemIcon, ListItemText, Avatar, Fab, IconButton } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import { tokens } from "../../theme";
 
 const Chat1 = () => {
-    const [users, setUsers] = useState([]);
-    const { id } = useParams(); // Get the user ID from the route
-    const currentUserIndex = parseInt(id);
-    useEffect(() => {
-        // Fetch users from the backend
-        axios.get('http://localhost:3000/api/users')
-            .then(response => {
-                // Map the response data to the desired structure
-                const mappedUsers = response.data.map(user => ({
-                    id: user._id,
-                    name: user.firstName,
-                    avatar: user.avatar
-                }));
-    
-                // Find the index of the user with the ID matching currentUserIndex
-                const currentUserIndexInArray = mappedUsers.findIndex(user => user.id === currentUserIndex);
-    
-                // If the user is found, move them to the beginning of the array
-                if (currentUserIndexInArray !== -1) {
-                    const currentUser = mappedUsers.splice(currentUserIndexInArray, 1)[0];
-                    mappedUsers.unshift(currentUser);
-                }
-    
-                setUsers(mappedUsers);
-                console.log(mappedUsers);
-            })
-            .catch(error => {
-                console.error("An error occurred while fetching user data:", error);
-            });
-    }, []);
-    
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [users, setUsers] = useState([]);
+  const { id } = useParams(); // Get the user ID from the route
+  const currentUserIndex = parseInt(id);
+  useEffect(() => {
+      // Fetch users from the backend
+      axios.get('http://localhost:3000/api/users')
+          .then(response => {
+              // Map the response data to the desired structure
+              const mappedUsers = response.data.map(user => ({
+                  id: user._id,
+                  name: user.firstName,
+                  avatar: user.avatar
+              }));
+  
+              // Find the index of the user with the ID matching currentUserIndex
+              const currentUserIndexInArray = mappedUsers.findIndex(user => user.id === currentUserIndex);
+  
+              // If the user is found, move them to the beginning of the array
+              if (currentUserIndexInArray !== -1) {
+                  const currentUser = mappedUsers.splice(currentUserIndexInArray, 1)[0];
+                  mappedUsers.unshift(currentUser);
+              }
+  
+              setUsers(mappedUsers);
+              console.log(mappedUsers);
+          })
+          .catch(error => {
+              console.error("An error occurred while fetching user data:", error);
+          });
+  }, []);
 
-    const messageIdGenerator = (message) => nanoid();
-    const groupIdGenerator = () => nanoid();
 
-    const storages = users.map(() => new BasicStorage({ groupIdGenerator, messageIdGenerator }));
-    const serviceFactory = (storage, updateState) => new ExampleChatService(storage, updateState);
-
-    const chatUsers = users.map((user, index) => new User({
-        i: user.id,
-        id: user.name,
-        presence: new Presence({ status: UserStatus.Available, description: "" }),
-        firstName: "",
-        lastName: "",
-        username: user.name,
-        email: "",
-        avatar: user.avatar,
-        bio: ""
-    }));
-
-    const chats = users.map((user, index) => ({
-        name: user.name,
-        storage: storages[index]
-    }));
-    
-
-    function createConversation(id, name) {
-        return new Conversation({
-            id,
-            participants: [
-                new Participant({
-                    id: name,
-                    role: new ConversationRole([])
-                })
-            ],
-            unreadCounter: 0,
-            typingUsers: new TypingUsersList({items: []}),
-            draft: ""
-        });
-    }
-    
-    // Add users and conversations to the states
-    chats.forEach(c => {
-    
-        users.forEach(u => {
-            if (u.name !== c.name) {
-                c.storage.addUser(new User({
-                    id: u.name,
-                    presence: new Presence({status: UserStatus.Available, description: ""}),
-                    firstName: "",
-                    lastName: "",
-                    username: u.name,
-                    email: "",
-                    avatar: u.avatar,
-                    bio: ""
-                }));
-    
-                const conversationId = nanoid();
-    
-                const myConversation = c.storage.getState().conversations.find(cv => typeof cv.participants.find(p => p.id === u.name) !== "undefined");
-                if (!myConversation) {
-    
-                    c.storage.addConversation(createConversation(conversationId, u.name));
-    
-                    const chat = chats.find(chat => chat.name === u.name);
-    
-                    if (chat) {
-    
-                        const hisConversation = chat.storage.getState().conversations.find(cv => typeof cv.participants.find(p => p.id === c.name) !== "undefined");
-                        if (!hisConversation) {
-                            chat.storage.addConversation(createConversation(conversationId, c.name));
-                        }
-    
-                    }
-    
-                }
-    
+  return (
+    <Box m="20px">
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant="h5">Chat</Typography>
+        </Grid>
+      </Grid>
+      <Paper>
+        <Grid container sx={{ height: "87vh" }}>
+          <Grid item xs={3} sx={{ borderRight: "1px solid #e0e0e0" }}>
+            <List>
+            {users && users.length > 0 &&
+              <ListItem button key={users[0].id}>
+                <ListItemIcon>
+                  <Avatar
+                    alt={users[0].name}
+                    src={users[0].avatar}
+                  />
+                </ListItemIcon>
+                <ListItemText primary={users[0].name}></ListItemText>
+              </ListItem>
             }
-        });
-    
-    });
-    
-    return (
-        <Box height="92%">
-            {chats.map((chat, index) => (
-                <ChatProvider
-                    key={index}
-                    serviceFactory={serviceFactory}
-                    storage={chat.storage}
-                    config={{
-                        typingThrottleTime: 250,
-                        typingDebounceTime: 900,
-                        debounceTyping: true,
-                        autoDraft: AutoDraft.Save | AutoDraft.Restore
-                    }}
-                >
-                    <Chat user={chatUsers[index]}/>
-                </ChatProvider>
-            ))}
-        </Box>
-    );
-    
-}
+            </List>
+            <Divider />
+            <Grid item xs={12} sx={{ padding: "10px" }}>
+              <TextField id="outlined-basic-email" label="Search" variant="outlined" fullWidth />
+            </Grid>
+            <Divider />
+            <List>
+            {users && users.length > 0 && users.map(user =>
+            {
+              if (user.id !== currentUserIndex) {
+               return (<ListItem button key={user.id}>
+                <ListItemIcon>
+                 <Avatar
+                    alt={user.name}
+                    src={user.avatar}
+                  />
+                </ListItemIcon>
+                <ListItemText primary={user.name}></ListItemText>
+              </ListItem>)
+                }
+            }
+            )
+
+            }
+
+            </List>
+          </Grid>
+          <Grid item xs={9}>
+            <List sx={{ height: "77vh", overflowY: "auto" }}>
+              {/* Message List */}
+              <ListItem key="1">
+                <Grid container>
+                  <Grid item xs={12}>
+                    <ListItemText align="right" primary="Hey man, What's up ?"></ListItemText>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <ListItemText align="right" secondary="09:30"></ListItemText>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem key="2">
+                <Grid container>
+                  <Grid item xs={12}>
+                    <ListItemText align="left" primary="Hey, Iam Good! What about you ?"></ListItemText>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <ListItemText align="left" secondary="09:31"></ListItemText>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem key="3">
+                <Grid container>
+                  <Grid item xs={12}>
+                    <ListItemText align="right" primary="Cool. i am good, let's catch up!"></ListItemText>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <ListItemText align="right" secondary="10:30"></ListItemText>
+                  </Grid>
+                </Grid>
+              </ListItem>
+            </List>
+            <Divider />
+            <Grid container sx={{ padding: "20px" }}>
+              <Grid item xs={10}>
+                <TextField id="outlined-basic-email" label="Type Something" fullWidth />
+              </Grid>
+              <Grid item xs={2} align="right">
+                <IconButton color="secondary" aria-label="attach-file" sx={{
+                    fontSize: "14px",
+                    margin: "5px",
+                    }}>
+                  <AttachFileIcon />
+                </IconButton>
+                <Fab color="primary" aria-label="add"   sx={{
+                    backgroundColor: colors.blueAccent[700],
+                    color: colors.grey[100],
+                    padding: "10px 20px",
+                    margin: "5px",
+                }}>
+                  <SendIcon />
+                </Fab>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Box>
+  );
+};
 
 export default Chat1;
