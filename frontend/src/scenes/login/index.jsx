2cb55dd9ab471f5ce3bff8a2bc25ceda1f6b0232
med mk <med.mbarek.isi@gmail.com>
2023-09-06 00:25:56 +0200

    fix the login design and implement the remember me feature

--- frontend/src/scenes/login/index.jsx
+++ frontend/src/scenes/login/index.jsx
@@ -1,33 +1,45 @@
 import * as React from 'react';
 import axios from 'axios';
-import { useSignIn, useSignOut } from 'react-auth-kit';
+import { useEffect, useState } from 'react';
+import { useIsAuthenticated, useSignIn, useSignOut } from 'react-auth-kit';
 import { useNavigate } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-
-
-
+import { Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, Container, useTheme } from '@mui/material';
+import { tokens } from "../../theme";
 
 const SignIn = () => {
-  const signOut = useSignOut()
+  const [rememberMe, setRememberMe] = useState(false);
+  const [email, setEmail] = useState('');
+  const isAuthenticated = useIsAuthenticated();
   const signIn = useSignIn();
+  const signOut = useSignOut();
   const navigate = useNavigate();
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-  signOut();
+  useEffect(() => {
+    const rememberedEmail = localStorage.getItem('rememberedEmail');
+    if (rememberedEmail) {
+      setEmail(rememberedEmail);
+      setRememberMe(true);
+    }
+
+    if (isAuthenticated) {
+      signOut();
+    }
+  }, []);
+  
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     const email = data.get('email');
     const password = data.get('password');
+  
+    if (rememberMe) {
+      localStorage.setItem('rememberedEmail', email);
+    } else {
+      localStorage.removeItem('rememberedEmail');
+    }
 
     try {
       // Send a POST request to your API endpoint for authentication
@@ -53,64 +65,67 @@ const SignIn = () => {
   };
 
   return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-              </Grid>
-            </Grid>
-          </Box>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          backgroundColor: colors.primary[400],
+          padding: '20px',
+          borderRadius: '10px',
+        }}
+      >
+        <Typography component="h1" variant="h5" sx={{ color: colors.grey[100] }}>
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        autoFocus
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        sx={{
+          backgroundColor: colors.grey[200],
+        }}
+      />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            sx={{
+              backgroundColor: colors.grey[200],
+            }}
+          />
+        <FormControlLabel
+                control={<Checkbox checked={rememberMe} onChange={() => setRememberMe(!rememberMe)} value="remember" color="primary" />}
+                label="Remember me"
+                sx={{ color: colors.grey[100] }}
+              />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, backgroundColor: colors.blueAccent[700], color: colors.grey[100] }}
+          >
+            Sign In
+          </Button>
         </Box>
-      </Container>
+      </Box>
+    </Container>
   );
 }
 
